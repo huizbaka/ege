cd1cdf1cf12dd141bcce2e2a3cf0a8f394af50f4
huizbaka <zebragepard@mail.ru>
2024-09-10 19:17:55 +0300

    commit

---
+++ '''num = int(input()).py
@@ -0,0 +1,16 @@
+'''num = int(input())
+m = sum(list(map(int, str(i))))
+print(m)'''
+
+
+'''for n in range (1,100):
+    s = bin(n)[2:]
+    if s.count('1') % 2 == 0:
+        s += '00'
+    else:
+        s += '10'
+    r = int(s, 2)
+    if r>43:
+        print(r)
+        break'''
+
